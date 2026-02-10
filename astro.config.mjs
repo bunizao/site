dc9e47f6c3db0e9703c28a627d83e7613ddd61fd
bunizao <102936102+bunizao@users.noreply.github.com>
2026-02-10 18:53:00 +0800

    chore: update Astro configuration for improved build performance and HTML compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,8 +11,14 @@ export default defineConfig({
   ],
   site: 'https://buxx.me',
   output: 'static',
+  compressHTML: true,
   adapter: vercel(),
   vite: {
+    build: {
+      sourcemap: false,
+      minify: 'esbuild',
+      cssMinify: true,
+    },
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url)),
